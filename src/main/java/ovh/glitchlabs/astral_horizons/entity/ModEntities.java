e230554c2c57c301a99ca4228653fed0571420ae
Ian441337 <ian1337@outlook.de>
2025-05-10 22:40:26 +0200

    more

--- src/main/java/ovh/glitchlabs/astral_horizons/entity/ModEntities.java
+++ src/main/java/ovh/glitchlabs/astral_horizons/entity/ModEntities.java
@@ -1,11 +1,9 @@
 package ovh.glitchlabs.astral_horizons.entity;
 
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import ovh.glitchlabs.astral_horizons.astral_horizons;
 
